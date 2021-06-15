be05aa33c0695af3cd9763783724fb81b03f08c2
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-06-15 12:35:49 +0530

    WPF-58241 - Sample added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            scheduler.DaysViewSettings.SpecialTimeRegions.Add(new SpecialTimeRegion
+            {
+                StartTime = DateTime.Today.AddHours(13),
+                EndTime = DateTime.Today.AddHours(14),
+                Text = "Lunch",
+                CanEdit = false,
+                Background = Brushes.Black,
+                Foreground = Brushes.White,
+                CanMergeAdjacentRegions = true,
+                RecurrenceRule = "FREQ=DAILY;INTERVAL=1"
+            });
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.scheduler = null;
+        }
+    }
+}
